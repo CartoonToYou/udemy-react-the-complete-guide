5c7eae9ec15900865b58035f327863ef81b7919c
Chachchai Tongthavornsuwan <38347860+CartoonToYou@users.noreply.github.com>
2021-09-01 20:45:12 +0700

    feat: Lists and conditional
    Learn about how to use lists and conditional in React

--- 03_RenderingLists-ConditionalContent/src/components/Expenses/Expenses.js
+++ 03_RenderingLists-ConditionalContent/src/components/Expenses/Expenses.js
@@ -1,19 +1,41 @@
-import "./Expenses.css";
+import React, { useState } from "react";
 
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 import ExpenseItem from "./ExpenseItem";
 
+import "./Expenses.css";
+
 const Expenses = (props) => {
+  const [selectedFilter, setSelectedFilter] = useState(2020);
+
+  const selectedFilterHandler = (selectedYear) => {
+    setSelectedFilter(selectedYear);
+  };
+
+  const filterExpenses = props.expenses.filter(
+    (expense) => expense.date.getFullYear() === +selectedFilter
+  );
+
   return (
-    <div className="expenses">
-      {props.expenses.map((expense, idx) => (
-        <ExpenseItem
-          key={idx}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
-    </div>
+    <Card className="expenses">
+      <ExpensesFilter
+        select={selectedFilter}
+        onFilterYear={selectedFilterHandler}
+      />
+      {filterExpenses.length === 0 ? (
+        <p>No expenses found.</p>
+      ) : (
+        filterExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))
+      )}
+    </Card>
   );
 };
 
