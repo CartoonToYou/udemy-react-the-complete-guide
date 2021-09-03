7859f1b61d95825e44f45f7b33cbfab29d27ab93
Chachchai Tongthavornsuwan <89886557+Chachchai-Tong@users.noreply.github.com>
2021-09-03 22:00:48 +0700

    feat: done rendering lists and conditional in React

--- 03_RenderingLists-ConditionalContent/src/components/Expenses/Expenses.js
+++ 03_RenderingLists-ConditionalContent/src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
 
 import "./Expenses.css";
 
@@ -23,6 +24,7 @@ const Expenses = (props) => {
         select={selectedFilter}
         onFilterYear={selectedFilterHandler}
       />
+      <ExpensesChart expenses={filterExpenses} />
       <ExpensesList items={filterExpenses} />
     </Card>
   );
