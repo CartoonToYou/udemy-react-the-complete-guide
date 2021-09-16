93a954426af683302f7c6b2aa11b1ce2afea798f
Chachchai Tongthavornsuwan <89886557+Chachchai-Tong@users.noreply.github.com>
2021-09-16 09:25:13 +0700

    feat: done useReducer and useEffect, start createContext

--- 08_HandlingSideEff-UseReducer-UseContext/src/App.js
+++ 08_HandlingSideEff-UseReducer-UseContext/src/App.js
@@ -5,14 +5,14 @@ import Home from "./components/Home/Home";
 import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   useEffect(() => {
     const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
     if (storedUserLoggedInInformation === "1") {
       setIsLoggedIn(true);
     }
   }, []);
-
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
