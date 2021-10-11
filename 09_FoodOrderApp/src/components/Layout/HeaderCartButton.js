ff57ae8a50b6184c0c6242ad58e3be5bac1608de
Chachchai Tongthavornsuwan <chachchai.toon@gmail.com>
2021-10-11 09:27:52 +0700

    feat: Add Cart context & provider
    create context and provider file for using in app.js

--- 09_FoodOrderApp/src/components/Layout/HeaderCartButton.js
+++ 09_FoodOrderApp/src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import CardIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 import classes from "./HeaderCardButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCardItems = cartCtx.items.reduce((currNumber, item) => {
+    return currNumber + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CardIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCardItems}</span>
     </button>
   );
 };
