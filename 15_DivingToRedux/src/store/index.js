b5aaa436b57b3568fea5f8d0473fe4c9c233ecd1
Chachchai Tongthavornsuwan <chachchai_toon@hotmail.com>
2022-01-24 10:12:54 +0700

    feat: learn to use Redux to kit instead of Redux

--- 15_DivingToRedux/src/store/index.js
+++ 15_DivingToRedux/src/store/index.js
@@ -1,25 +1,65 @@
-import { createStore } from "redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
 
-const counterReducer = (state = { counter: 0 }, action) => {
-  if (action.type === "increment") {
-    return {
-      counter: state.counter + 1,
-    };
-  }
+const initialCounterState = { counter: 0, showCounter: true };
 
-  if (action.type === "increase") {
-    return { counter: state.counter + action.amount };
-  }
+/**
+ * (Redux) Reducer => can't mutatable all states, must create brand new obj and return only!!!
+ * (RTK) createSlice => can mutatable all states and RTK will complied them all to not mutation states
+ */
 
-  if (action.type === "decrement") {
-    return {
-      counter: state.counter - 1,
-    };
-  }
+const counterSlice = createSlice({
+  name: "counter",
+  initialState: initialCounterState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter += action.payload;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
-  return state;
-};
+const initialAuthState = { isAuthenticated: false };
 
-const store = createStore(counterReducer);
+const authSlice = createSlice({
+  name: "authentication",
+  initialState: initialAuthState,
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    },
+  },
+});
+
+/** Map of reducer
+ * -- USE THIS -- when need to use many reducers (RTK will merged all with this structure)
+ */
+const store = configureStore({
+  reducer: {
+    counter: counterSlice.reducer,
+    authentication: authSlice.reducer,
+    // ...restReducers
+  },
+});
+
+/**
+ * Global reducer in one object
+ * -- USE THIS -- when need to use just one object
+ */
+// const store = configureStore({
+//   reducer: counterSlice.reducer,
+// });
+
+export const counterActions = counterSlice.actions;
 
 export default store;
