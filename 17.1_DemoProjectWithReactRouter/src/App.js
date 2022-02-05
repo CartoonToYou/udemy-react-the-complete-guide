662c2480b1b5d3304a1ea7c04008b98f7e43e716
Chachchai Tongthavornsuwan <chachchai_toon@hotmail.com>
2022-02-05 09:16:53 +0700

    learn: start learning react router with demo project

---
+++ 17.1_DemoProjectWithReactRouter/src/App.js
@@ -0,0 +1,26 @@
+import { Redirect, Route, Switch } from "react-router-dom";
+
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
+
+function App() {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Redirect to="/quotes" />
+      </Route>
+      <Route exact path="/quotes">
+        <AllQuotes />
+      </Route>
+      <Route path="/quotes/:quoteId">
+        <QuoteDetail />
+      </Route>
+      <Route path="/new-quote">
+        <NewQuote />
+      </Route>
+    </Switch>
+  );
+}
+
+export default App;
