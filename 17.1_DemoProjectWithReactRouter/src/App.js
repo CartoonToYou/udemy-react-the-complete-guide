6e1ce3f08894a7339d9feaea5f26b2b1a64dfce3
Chachchai Tongthavornsuwan <chachchai_toon@hotmail.com>
2022-02-10 11:12:25 +0700

    Learn: Prompt before changing page

--- 17.1_DemoProjectWithReactRouter/src/App.js
+++ 17.1_DemoProjectWithReactRouter/src/App.js
@@ -1,25 +1,32 @@
 import { Redirect, Route, Switch } from "react-router-dom";
 
+import Layout from "./components/layout/Layout";
 import AllQuotes from "./pages/AllQuotes";
 import NewQuote from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        <Redirect to="/quotes" />
-      </Route>
-      <Route exact path="/quotes">
-        <AllQuotes />
-      </Route>
-      <Route path="/quotes/:quoteId">
-        <QuoteDetail />
-      </Route>
-      <Route path="/new-quote">
-        <NewQuote />
-      </Route>
-    </Switch>
+    <Layout>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/quotes" />
+        </Route>
+        <Route exact path="/quotes">
+          <AllQuotes />
+        </Route>
+        <Route path="/quotes/:quoteId">
+          <QuoteDetail />
+        </Route>
+        <Route path="/new-quote">
+          <NewQuote />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+      </Switch>
+    </Layout>
   );
 }
 
