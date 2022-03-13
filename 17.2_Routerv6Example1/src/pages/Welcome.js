97b6114c500bd84c6938351af04167cbe625f74b
Chachchai Tongthavornsuwan <chachchai_toon@hotmail.com>
2022-03-13 13:30:16 +0700

    Finished: React Route Dom v5 & v6

---
+++ 17.2_Routerv6Example1/src/pages/Welcome.js
@@ -0,0 +1,21 @@
+import {
+  // Routes, Route,
+  Link,
+  Outlet,
+} from "react-router-dom";
+
+const Welcome = () => {
+  return (
+    <section>
+      <h1>The Welcome Page</h1>
+
+      <Link to="new-user">New User</Link>
+      <Outlet />
+      {/* <Routes>
+        <Route path="new-user" element={<p>Welcome, new user!</p>} />
+      </Routes> */}
+    </section>
+  );
+};
+
+export default Welcome;
